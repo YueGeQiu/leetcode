92caa633e65d575b9b763c3f3a1c0e5e72db8b1f
answerwa <answerwa@126.com>
2018-12-01 00:03:04 +0800

    add problem #21 for C language

---
+++ 021/Merge_Two_Sorted_Lists.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    struct ListNode *p = &dummy;
+
+    dummy.next = NULL;
+
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            p->next = l1;
+            l1 = l1->next;
+        } else {
+            p->next = l2;
+            l2 = l2->next;
+        }
+        p = p->next;
+    }
+
+    if (l1) p->next = l1;
+    if (l2) p->next = l2;
+
+    return dummy.next;
+}
