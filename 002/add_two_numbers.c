3bf6a2045f786c8b81dbadd0aae0a9b5036b4e58
answerwa <answerwa@126.com>
2018-12-08 00:06:22 +0800

    optimization #002

--- 002/add_two_numbers.c
+++ 002/add_two_numbers.c
@@ -6,34 +6,29 @@
  * };
  */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    int tmp = 0;
-    struct ListNode *pHead, *p, *pl1, *pl2;
-    pHead = NULL;
-    if(l1 || l2 || tmp) {
-        p = (struct ListNode *)malloc(sizeof(struct ListNode));
-        pHead = p;
-        p->val = l1 ? l1->val : 0;
-        p->val += l2 ? l2->val : 0;
-        p->val += tmp;
-        tmp = p->val / 10;
-        p->val = p->val % 10;
-        pl1 = l1 ? l1->next : NULL;
-        pl2 = l2 ? l2->next : NULL;
-        p->next = NULL;
+    struct ListNode ret;
+    struct ListNode *p = &ret;
+    int carry = 0, sum;
+
+    ret.next = NULL;
+
+    while (l1 || l2 || carry != 0) {
+        sum = carry;
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        p->next = (struct ListNode *)calloc(1, sizeof(struct ListNode));
+        p = p->next;
+        p->val = sum % 10;
+        carry = sum / 10;
     }
 
-    while(pl1 || pl2 || tmp) {
-        p->next = (struct ListNode *)malloc(sizeof(struct ListNode));
-        p = p->next;
-        
-        p->val = pl1 ? pl1->val : 0;
-        p->val += pl2 ? pl2->val : 0;
-        p->val += tmp;
-        tmp = p->val / 10;
-        p->val = p->val % 10;
-        pl1 = pl1 ? pl1->next : NULL;
-        pl2 = pl2 ? pl2->next : NULL;
-        p->next = NULL;
-    }
-    return pHead;
+    return ret.next;
 }
