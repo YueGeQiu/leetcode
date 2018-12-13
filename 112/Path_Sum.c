9877b50586be240d76404f8f477af89efd001a56
answerwa <answerwa@126.com>
2018-12-13 22:37:49 +0800

    optimization #112

--- 112/Path_Sum.c
+++ 112/Path_Sum.c
@@ -7,15 +7,10 @@
  * };
  */
 bool hasPathSum(struct TreeNode* root, int sum) {
-    if (!root) {
-        return false;
-    }
-    
-    int val = sum - root->val;
-    
-    if (val == 0 && !root->left && !root->right) {
-        return true;
-    }
-    
-    return hasPathSum(root->left, val) || hasPathSum(root->right, val);
+    if (!root) return false;
+
+    if (!root->left && !root->right && root->val == sum) return true;
+
+    return hasPathSum(root->left, sum - root->val) ||
+             hasPathSum(root->right, sum - root->val);
 }
