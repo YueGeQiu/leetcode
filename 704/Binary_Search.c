deeb9b92d674f24a116205e967b6400608a843d9
answerwa <answerwa@126.com>
2019-02-06 21:05:03 +0800

    add problem #704

---
+++ 704/Binary_Search.c
@@ -0,0 +1,12 @@
+int search(int* nums, int numsSize, int target) {
+    if (!nums) return -1;
+    int left = 0, right = numsSize - 1, mid;
+
+    while(left <= right) {
+        mid = left + (right - left) / 2;
+        if (target < nums[mid]) right = mid - 1;
+        else if (target > nums[mid]) left = mid + 1;
+        else return mid;
+    }
+    return -1;
+}
